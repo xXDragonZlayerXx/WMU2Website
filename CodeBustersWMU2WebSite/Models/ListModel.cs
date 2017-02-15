332aac255c23e49277835cecc775b06bad6e6773
Administrator <Administrator@COMPUTER>
2017-02-15 13:18:03 +0100

    Add project files.

---
+++ CodeBustersWMU2WebSite/Models/ListModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeBustersWMU2WebSite.Models
+{
+    public class Tasks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string BeginDateTime { get; set; }
+        [MaxLength(50)]
+        public string DeadlineDateTime { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Requirements { get; set; }
+
+    }
+}
